d91e21604311fe20f1e6954316df1fe71880b3ba
Matt Beedle <mattbeedle@googlemail.com>
2013-12-23 10:47:55 +0100

    First commit

---
+++ lib/usda/nutrient/database.rb
@@ -0,0 +1,9 @@
+require "usda/nutrient/database/version"
+
+module Usda
+  module Nutrient
+    module Database
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/usda/nutrient/database/version.rb
@@ -0,0 +1,7 @@
+module Usda
+  module Nutrient
+    module Database
+      VERSION = "0.0.1"
+    end
+  end
+end
