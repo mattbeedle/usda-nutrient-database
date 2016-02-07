0bb0eb323d966a42af57c87eb7606bd77f54434c
mattbeedle <mattbeedle@gmail.com>
2016-02-07 12:17:56 +0100

    Fix referential integrity

--- db/migrate/9_add_referential_integrity.rb
+++ db/migrate/9_add_referential_integrity.rb
@@ -9,6 +9,46 @@ class AddReferentialIntegrity < ActiveRecord::Migration
     remove_index :usda_food_groups, :code
     add_index :usda_food_groups, :code, unique: true
 
+    remove_index :usda_foods, :nutrient_databank_number
+    add_index :usda_foods, :nutrient_databank_number, unique: true
+
+    add_index :usda_foods_nutrients, [
+      :nutrient_databank_number,
+      :nutrient_number
+    ],
+    unique: true,
+    name: "index_usda_foods_nutrients_on_databank_number_and_number"
+
+    add_foreign_key :usda_foods,
+      :usda_food_groups,
+      column: :food_group_code,
+      primary_key: :code
+
+    add_foreign_key :usda_foods_nutrients,
+      :usda_foods,
+      column: :nutrient_databank_number,
+      primary_key: :nutrient_databank_number
+
+    add_foreign_key :usda_foods_nutrients,
+      :usda_nutrients,
+      column: :nutrient_number,
+      primary_key: :nutrient_number
+
+    add_foreign_key :usda_foods_nutrients,
+      :usda_source_codes,
+      column: :src_code,
+      primary_key: :code
+
+    add_foreign_key :usda_footnotes,
+      :usda_foods,
+      column: :nutrient_databank_number,
+      primary_key: :nutrient_databank_number
+
+    add_foreign_key :usda_weights,
+      :usda_foods,
+      column: :nutrient_databank_number,
+      primary_key: :nutrient_databank_number
+
     add_foreign_key :usda_foods,
       :usda_food_groups,
       column: :food_group_code,
